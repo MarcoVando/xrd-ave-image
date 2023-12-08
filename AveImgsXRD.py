1bc7993feaed5376ca9537d6b01f3fadf5657222
Marco Vandone <servizi.marco@gmail.com>
2023-12-07 21:42:43 -0500

    Update AveImgsXRD.py

--- AveImgsXRD.py
+++ AveImgsXRD.py
@@ -8,24 +8,28 @@ import sys
 import glob
 import subprocess
 
-IMG_EXT = "*.tiff"
 SUBDIR = './average/'
+
+img_ext = "*.tiff"
+method = 'mean'  #the method for pyFAI-average
 
 if sys.argv[1] == '-h' or sys.argv[1] == '--help':
     print("The program read all the images with the user-specified format in the folder and it averages them. User can also specify the pyfai averaging method (default is mean)")
     print("program takes the following arguments in the following formats:")
-    print("<.extension of the images to integrate> </> <averaging method>")
+    print("<.extension of the images to integrate> </> <averaging method (optional)>")
     sys.exit(1)
+
+if len(sys.argv) == 2:
+    method = sys.argv[3]
+img_ext = sys.argv[2]
     
-read_img = glob.glob(IMG_EXT)
+read_img = glob.glob(img_ext)
 
 print(f"found {len(read_img)} images")
 for x in read_img:
    print(x)
 
 file_string = ' '.join(read_img)   #compose the FILENAME argument for pyFAI-average
-
-method = 'mean'  #the method for pyFAI-average
 
 try:
    os.mkdir(SUBDIR)
